01f7451e27278ddafab6fbf04b1937def21f7bda
MariannePV <mariannepulgarvega12@gmail.com>
2024-05-03 19:03:23 +0200

    MainWindow MVVM v1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ThumbLedge
         {
             InitializeComponent();
         }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
